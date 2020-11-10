59cbb9fb7c569f2f933f67c1bd8fab1505562cb6
iman <67876839+imandurr@users.noreply.github.com>
2020-11-10 14:32:05 -0500

    Add files via upload
    Check in 1

---
+++ final.py
@@ -0,0 +1,50 @@
+class Account:
+    """Create an account that will either be used to be a customer or an online retailer. """
+    def __init__(self, account_type, username, birthday, address, email, password):
+        
+    def change_login_credentials():
+        """ Purpose: to edit login credentials (update email, chang username, etc. Args: login information. """
+        
+
+class Product:
+    """Creates a product  to be listed in the store with the vendor name, a description, and the price"""
+    def __init__(self, desc, price, reviews, inventory_size):
+        
+    def set_product_information():
+        """Sets the product object with the given name, description, price, and rating"""
+        
+    
+    
+class Inventory:
+    """Provides a list of all the products in the store with the amount left available for each product"""
+    def __init__(self):
+    
+    def add_inventory():
+        """Purpose: Add products to inventory. Args: Product(a product object): product that’s being added to the inventory.Quantity(int): quantity of a product. Returns: List of products with newly added values. """ 
+        
+    def change_inventory():
+        """ Removing or editing the inventory. Args: Product(a product object): product that’s being edited. Quantity(int: updated quantity of a product. Returns: An updated list of inventory. """
+    
+        
+class Cart:
+    """Calculate final checkout price of the order with the option to add or remove from cart and to add a discount code. """
+    def __init__(self):
+        
+    def add_to_cart():
+        """"Adds products from shopping cart Returns: An updated shopping cart """
+    
+    def remove_from_cart():
+        """"Removes products from shopping cart Returns: An updated shopping cart """
+        
+    def cost(discount, shipping, tax):
+        """Determines the checkout price for the order by including the shipping cost and discount Args: discount codes, Shipping cost. Returns: Float of the final checkout price"""
+
+    def currency_exchange(price):
+        """ Returns the final price of the item in the customer's currency. Args: Original Price, Final price in desired currency."""
+        
+    
+        
+    
+    
+
+    
